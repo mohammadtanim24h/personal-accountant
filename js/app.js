e14eee7326338f4b3f788c9e905de74323f14e0e
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-02-17 14:49:29 +0600

    saving section error handled

--- js/app.js
+++ js/app.js
@@ -13,29 +13,26 @@ function inputError() {
 
 // calculate total expenses and balance
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    debugger;
     const incomeAmount = getInputValue('income-input');
     const foodCost = getInputValue('food-cost');
     const rentCost = getInputValue('rent-cost');
     const clothesCost = getInputValue('cloth-cost');
-    // invalid input handling
+    // empty input field value handling
     if (isNaN(incomeAmount) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothesCost)) {
-        // alert('please enter numbers in the input fields');
         document.getElementById('error-message').innerText = 'Input fields can not be empty';
         inputError();
         return;
     }
     // negative input handling
     if (incomeAmount < 0 || foodCost < 0 || rentCost < 0 || clothesCost < 0) {
-        // alert('please enter positive numbers in the input fields');
         document.getElementById('error-message').innerText = 'Please Enter positive numbers';
         inputError();
         return;
     }
     // total cost
     const totalCost = foodCost + rentCost + clothesCost;
+    // total expense more than income error handling
     if (totalCost > incomeAmount) {
-        // alert('total expense cant be more than your income');
         document.getElementById('error-message').innerText = 'Total Expense can not be more than your Income';
         inputError();
         return;
@@ -61,6 +58,7 @@ function getAmount (elementId) {
 
 document.getElementById('save-btn').addEventListener('click', function () {
     // calculating saving amount
+    debugger;
     const savingPercentage = getInputValue('save-input') / 100;
     const incomeAmount = getInputValue('income-input');
     const savingAmount = incomeAmount * savingPercentage;
@@ -68,8 +66,13 @@ document.getElementById('save-btn').addEventListener('click', function () {
     const balanceAmount = getAmount('balance');
     // error handling when saving amount is more than balance
     if (savingAmount > balanceAmount) {
-        alert('you cant save more than your balance');
+        // alert('you cant save more than your balance');
+        document.getElementById('save-error-message').innerText = 'You can not save more than your balance';
+        document.getElementById('saving-error').style.display = 'block';
         return;
+    }
+    else {
+        document.getElementById('saving-error').style.display = 'none';
     }
     // updating saving amount
     const savingAmountText = document.getElementById('saving-amount');
